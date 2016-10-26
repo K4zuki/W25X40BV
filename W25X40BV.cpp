161dd7237ec5d72746023ba28b383548d1a7d3f9
Kazuki Yamamoto <k.yamamoto.08136891@gmail.com>
2016-10-26 22:16:51 +0900

    apply MIT license; apply clang-format formatting

--- W25X40BV.cpp
+++ W25X40BV.cpp
@@ -1,159 +1,179 @@
+/* Copyright (c) 2016 Kazuki Yamamoto <k.yamamoto.08136891@gmail.com>
+Permission is hereby granted, free of charge, to any person obtaining
+a copy of this software and associated documentation files
+(the "Software"), to deal in the Software without restriction,
+including without limitation the rights to use, copy, modify, merge,
+publish, distribute, sublicense, and/or sell copies of the Software,
+and to permit persons to whom the Software is furnished to do so,
+subject to the following conditions:
+The above copyright notice and this permission notice shall be
+included in all copies or substantial portions of the Software.
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY
+CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
+TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
+SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+*/
 /** W25X40BV.cpp
 */
 #include "W25X40BV.h"
 #include "SWSPI.h"
 
-//! CONSTRUCTOR 
-W25X40BV::W25X40BV(PinName mosi, PinName miso, PinName sclk, PinName cs) : SWSPI(mosi, miso, sclk), _cs(cs) {
-    this->format(SPI_NBIT, SPI_MODE);
-    this->frequency(SPI_FREQ);
-    chipDisable();
+//! CONSTRUCTOR
+W25X40BV::W25X40BV(PinName mosi, PinName miso, PinName sclk, PinName cs)
+    : SWSPI(mosi, miso, sclk), _cs(cs) {
+  this->format(SPI_NBIT, SPI_MODE);
+  this->frequency(SPI_FREQ);
+  chipDisable();
 }
-
 
 //! READING
 uint32_t W25X40BV::readByte(uint32_t addr) {
-    chipEnable();
-    this->write(R_INST);
-    this->write((addr & ADDR_BMASK2) >> ADDR_BSHIFT2);
-    this->write((addr & ADDR_BMASK1) >> ADDR_BSHIFT1);
-    this->write((addr & ADDR_BMASK0) >> ADDR_BSHIFT0);
-    uint32_t response = this->write(DUMMY_ADDR);
-    chipDisable();
-    return response;
+  chipEnable();
+  this->write(R_INST);
+  this->write((addr & ADDR_BMASK2) >> ADDR_BSHIFT2);
+  this->write((addr & ADDR_BMASK1) >> ADDR_BSHIFT1);
+  this->write((addr & ADDR_BMASK0) >> ADDR_BSHIFT0);
+  uint32_t response = this->write(DUMMY_ADDR);
+  chipDisable();
+  return response;
 }
+
 uint32_t W25X40BV::readByte(uint32_t a2, uint32_t a1, uint32_t a0) {
-   chipEnable();
-   this->write(R_INST);
-   this->write(a2);
-   this->write(a1);
-   this->write(a0);
-   uint32_t response = this->write(DUMMY_ADDR);
-    chipDisable();
-    return response;
+  chipEnable();
+  this->write(R_INST);
+  this->write(a2);
+  this->write(a1);
+  this->write(a0);
+  uint32_t response = this->write(DUMMY_ADDR);
+  chipDisable();
+  return response;
 }
+
 void W25X40BV::readStream(uint32_t addr, uint8_t* buf, uint32_t count) {
-    if (count < 1)
-        return;
-    chipEnable();
-    this->write(R_INST);
-    this->write((addr & ADDR_BMASK2) >> ADDR_BSHIFT2);
-    this->write((addr & ADDR_BMASK1) >> ADDR_BSHIFT1);
-    this->write((addr & ADDR_BMASK0) >> ADDR_BSHIFT0);
-    for (uint32_t i = 0; i < count; i++)
-        buf[i] =  this->write(DUMMY_ADDR);
-    chipDisable();
+  if (count < 1) return;
+  chipEnable();
+  this->write(R_INST);
+  this->write((addr & ADDR_BMASK2) >> ADDR_BSHIFT2);
+  this->write((addr & ADDR_BMASK1) >> ADDR_BSHIFT1);
+  this->write((addr & ADDR_BMASK0) >> ADDR_BSHIFT0);
+  for (uint32_t i = 0; i < count; i++) buf[i] = this->write(DUMMY_ADDR);
+  chipDisable();
 }
 
 // WRITING
 void W25X40BV::writeByte(uint32_t addr, uint32_t data) {
-    writeEnable();
-    chipEnable();
-    this->write(W_INST);
-    this->write((addr & ADDR_BMASK2) >> ADDR_BSHIFT2);
-    this->write((addr & ADDR_BMASK1) >> ADDR_BSHIFT1);
-    this->write((addr & ADDR_BMASK0) >> ADDR_BSHIFT0);
-    this->write(data);
-    chipDisable();
-    writeDisable();
-    wait_us(10*WAIT_TIME);
+  writeEnable();
+  chipEnable();
+  this->write(W_INST);
+  this->write((addr & ADDR_BMASK2) >> ADDR_BSHIFT2);
+  this->write((addr & ADDR_BMASK1) >> ADDR_BSHIFT1);
+  this->write((addr & ADDR_BMASK0) >> ADDR_BSHIFT0);
+  this->write(data);
+  chipDisable();
+  writeDisable();
+  wait_us(10 * WAIT_TIME);
 }
+
 void W25X40BV::writeByte(uint32_t a2, uint32_t a1, uint32_t a0, uint32_t data) {
-    writeEnable();
-    chipEnable();
-    this->write(W_INST);
-    this->write(a2);
-    this->write(a1);
-    this->write(a0);
-    this->write(data);
-    chipDisable();
-    writeDisable();
-    wait_us(10*WAIT_TIME);
+  writeEnable();
+  chipEnable();
+  this->write(W_INST);
+  this->write(a2);
+  this->write(a1);
+  this->write(a0);
+  this->write(data);
+  chipDisable();
+  writeDisable();
+  wait_us(10 * WAIT_TIME);
 }
+
 void W25X40BV::writeStream(uint32_t addr, uint8_t* buf, uint32_t count) {
-    if (count < 1)
-        return;
-    writeEnable();
-    chipEnable();
-    this->write(W_INST);
-    this->write((addr & ADDR_BMASK2) >> ADDR_BSHIFT2);
-    this->write((addr & ADDR_BMASK1) >> ADDR_BSHIFT1);
-    this->write((addr & ADDR_BMASK0) >> ADDR_BSHIFT0);
-    for (uint32_t i = 0; i < count; i++)
-        this->write(buf[i]);
-    chipDisable();
-    wait_ms(10*WAIT_TIME);
-    writeDisable();
+  if (count < 1) return;
+  writeEnable();
+  chipEnable();
+  this->write(W_INST);
+  this->write((addr & ADDR_BMASK2) >> ADDR_BSHIFT2);
+  this->write((addr & ADDR_BMASK1) >> ADDR_BSHIFT1);
+  this->write((addr & ADDR_BMASK0) >> ADDR_BSHIFT0);
+  for (uint32_t i = 0; i < count; i++) this->write(buf[i]);
+  chipDisable();
+  wait_ms(10 * WAIT_TIME);
+  writeDisable();
 }
 
-//ERASING
-void W25X40BV::pageErase(uint16_t page){
-    int msb = page>>8;
-    writeEnable();
-    chipEnable();
-    this->write(P_ERASE_INST);
-    this->write(msb);
-    this->write((int)page);
-    this->write(DUMMY_ADDR);
-    chipDisable();
-    writeDisable();
-    wait_ms(50*WAIT_TIME);
+// ERASING
+void W25X40BV::pageErase(uint16_t page) {
+  int msb = page >> 8;
+  writeEnable();
+  chipEnable();
+  this->write(P_ERASE_INST);
+  this->write(msb);
+  this->write((int)page);
+  this->write(DUMMY_ADDR);
+  chipDisable();
+  writeDisable();
+  wait_ms(50 * WAIT_TIME);
 }
 
-void W25X40BV::block4Erase(uint16_t block){
-    uint8_t msb = (uint8_t)(block >> 4);
-    block = (block << 4) & 0xF0;
-    writeEnable();
-    chipEnable();
-    this->write(B4K_ERASE_INST);
-    this->write((int)msb);
-    this->write((int)block);
-    this->write(DUMMY_ADDR);
-    chipDisable();
-    writeDisable();
-    wait_ms(100*WAIT_TIME);
+void W25X40BV::block4Erase(uint16_t block) {
+  uint8_t msb = (uint8_t)(block >> 4);
+  block = (block << 4) & 0xF0;
+  writeEnable();
+  chipEnable();
+  this->write(B4K_ERASE_INST);
+  this->write((int)msb);
+  this->write((int)block);
+  this->write(DUMMY_ADDR);
+  chipDisable();
+  writeDisable();
+  wait_ms(100 * WAIT_TIME);
 }
 
-void W25X40BV::block32Erase(uint8_t block){
-    block = (block << 3) & 0x18;
-    writeEnable();
-    chipEnable();
-    this->write(B32K_ERASE_INST);
-    this->write((int)block);
-    this->write(DUMMY_ADDR);
-    this->write(DUMMY_ADDR);
-    chipDisable();
-    writeDisable();
-    wait(WAIT_TIME);
+void W25X40BV::block32Erase(uint8_t block) {
+  block = (block << 3) & 0x18;
+  writeEnable();
+  chipEnable();
+  this->write(B32K_ERASE_INST);
+  this->write((int)block);
+  this->write(DUMMY_ADDR);
+  this->write(DUMMY_ADDR);
+  chipDisable();
+  writeDisable();
+  wait(WAIT_TIME);
 }
 
 void W25X40BV::chipErase() {
-    writeEnable();
-    chipEnable();
-    this->write(C_ERASE_INST);
-    chipDisable();
-    writeDisable();
-    wait(3*WAIT_TIME);
+  writeEnable();
+  chipEnable();
+  this->write(C_ERASE_INST);
+  chipDisable();
+  writeDisable();
+  wait(3 * WAIT_TIME);
 }
-    
 
-//ENABLE/DISABLE (private functions)
+// ENABLE/DISABLE (private functions)
 void W25X40BV::writeEnable() {
-    chipEnable();
-    this->write(WE_INST);
-    chipDisable();
+  chipEnable();
+  this->write(WE_INST);
+  chipDisable();
 }
+
 void W25X40BV::writeDisable() {
-    chipEnable();
-    this->write(WD_INST);
-    chipDisable();
+  chipEnable();
+  this->write(WD_INST);
+  chipDisable();
 }
+
 void W25X40BV::chipEnable() {
-    _cs.output();
-    _cs = 0;
+  _cs.output();
+  _cs = 0;
 }
+
 void W25X40BV::chipDisable() {
-    _cs = 1;
-    _cs.input();
-    _cs.mode(PullNone);
+  _cs = 1;
+  _cs.input();
+  _cs.mode(PullNone);
 }
--- W25X40BV.h
+++ W25X40BV.h
@@ -1,90 +1,110 @@
+/* Copyright (c) 2016 Kazuki Yamamoto <k.yamamoto.08136891@gmail.com>
+Permission is hereby granted, free of charge, to any person obtaining
+a copy of this software and associated documentation files
+(the "Software"), to deal in the Software without restriction,
+including without limitation the rights to use, copy, modify, merge,
+publish, distribute, sublicense, and/or sell copies of the Software,
+and to permit persons to whom the Software is furnished to do so,
+subject to the following conditions:
+The above copyright notice and this permission notice shall be
+included in all copies or substantial portions of the Software.
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY
+CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
+TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
+SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+*/
 /** W25X40BV.h
 */
 
-#ifndef W25X40BV_H
-#define W25X40BV_H
+#ifndef W25X40BV_H_
+#define W25X40BV_H_
 
-#include "mbed.h"
 #include "SWSPI.h"
+#include "mbed.h"  // NOLINT
 
-#define SPI_FREQ        10000000
-#define SPI_MODE        0
-#define SPI_NBIT        8
+#define SPI_FREQ 10000000
+#define SPI_MODE 0
+#define SPI_NBIT 8
 
-#define WE_INST         0x06
-#define WD_INST         0x04
-#define R_INST          0x03
-#define W_INST          0x02
-#define C_ERASE_INST    0x60
-#define P_ERASE_INST    0x81
-#define B4K_ERASE_INST  0x20
-#define B32K_ERASE_INST     0x52
+#define WE_INST 0x06
+#define WD_INST 0x04
+#define R_INST 0x03
+#define W_INST 0x02
+#define C_ERASE_INST 0x60
+#define P_ERASE_INST 0x81
+#define B4K_ERASE_INST 0x20
+#define B32K_ERASE_INST 0x52
 
-#define DUMMY_ADDR      0xAA
-#define WAIT_TIME       2
+#define DUMMY_ADDR 0xAA
+#define WAIT_TIME 2
 
-#define ADDR_BMASK2     0x001f0000
-#define ADDR_BMASK1     0x0000ff00
-#define ADDR_BMASK0     0x000000ff
+#define ADDR_BMASK2 0x001f0000
+#define ADDR_BMASK1 0x0000ff00
+#define ADDR_BMASK0 0x000000ff
 
-#define ADDR_BSHIFT2    16
-#define ADDR_BSHIFT1    8
-#define ADDR_BSHIFT0    0
+#define ADDR_BSHIFT2 16
+#define ADDR_BSHIFT1 8
+#define ADDR_BSHIFT0 0
 
 /**
 W25X40BV
 */
-class W25X40BV: public SWSPI {
-public:
-    W25X40BV(PinName mosi, PinName miso, PinName sclk, PinName cs);
-    
-    //! takes a 24-bit (3 bytes) address and returns the data (1 byte) at that location
-    uint32_t readByte(uint32_t addr);
-    //! takes the address in 3 separate bytes A[23,16], A[15,8], A[7,0]
-    uint32_t readByte(uint32_t a2, uint32_t a1,uint32_t a0);
-    //! takes a 24-bit address, reads count bytes, and stores results in buf
-    void readStream(uint32_t addr, uint8_t* buf, uint32_t count);
+class W25X40BV : public SWSPI {
+ public:
+  W25X40BV(PinName mosi, PinName miso, PinName sclk, PinName cs);
 
-    //! takes a 24-bit (3 bytes) address and a byte of data to write at that location
-    void writeByte(uint32_t addr, uint32_t data);
-    //! takes the address in 3 separate bytes A[23,16], A[15,8], A[7,0]
-    void writeByte(uint32_t a2, uint32_t a1, uint32_t a0, uint32_t data);
-    /** write count bytes of data from buf to memory, starting at addr
-    * addr is expected to be 256 byte alignment: 0x000000,0x000100,...,0x01FF00
-    * even if not, it works anyway. but should care count and 256byte edge: 
-    * if count=3 while addr=0x????FE, 3rd byte will be written in 0x????00
-    * @param addr 24 bit address
-    * @param buf data buffer
-    * @param count data count in byte
-    */
-    void writeStream(uint32_t addr, uint8_t* buf, uint32_t count);
-    
-    //! erase a page data(256bytes) on chip
-    void pageErase(uint16_t page);
+  //! takes a 24-bit (3 bytes) address and returns the data (1 byte) at that
+  //! location
+  uint32_t readByte(uint32_t addr);
+  //! takes the address in 3 separate bytes A[23,16], A[15,8], A[7,0]
+  uint32_t readByte(uint32_t a2, uint32_t a1, uint32_t a0);
+  //! takes a 24-bit address, reads count bytes, and stores results in buf
+  void readStream(uint32_t addr, uint8_t* buf, uint32_t count);
 
-    /** erase a block data(4Kbytes) on chip
-    * @param block block number 0x0000 - 0x001F
-    * block number will be left-shifted by 4 bits: 0x0000 - 0x01F0
-    * and a dummy address byte (0x00) will be added
-    */
-    void block4Erase(uint16_t block);
-    
-    /** erase a block data(32Kbytes) on chip
-    * @param block number 0x00 - 0x03
-    * block number will be left-shifted by 3 bits: 0x00 - 0x18
-    * and two dummy address bytes (0x0000) will be added
-    */
-    void block32Erase(uint8_t block);
-    void chipErase();                                       //! erase all data on chip
-    
-private:
-    void writeEnable();                                     //! write enable
-    void writeDisable();                                    //! write disable
-    void chipEnable();                                      //! chip enable
-    void chipDisable();                                     //! chip disable
-    
-   //! SPI _spi;
-    DigitalInOut _cs;
+  //! takes a 24-bit (3 bytes) address and a byte of data to write at that
+  //! location
+  void writeByte(uint32_t addr, uint32_t data);
+  //! takes the address in 3 separate bytes A[23,16], A[15,8], A[7,0]
+  void writeByte(uint32_t a2, uint32_t a1, uint32_t a0, uint32_t data);
+  /** write count bytes of data from buf to memory, starting at addr
+  * addr is expected to be 256 byte alignment: 0x000000,0x000100,...,0x01FF00
+  * even if not, it works anyway. but should care count and 256byte edge:
+  * if count=3 while addr=0x????FE, 3rd byte will be written in 0x????00
+  * @param addr 24 bit address
+  * @param buf data buffer
+  * @param count data count in byte
+  */
+  void writeStream(uint32_t addr, uint8_t* buf, uint32_t count);
+
+  //! erase a page data(256bytes) on chip
+  void pageErase(uint16_t page);
+
+  /** erase a block data(4Kbytes) on chip
+  * @param block block number 0x0000 - 0x001F
+  * block number will be left-shifted by 4 bits: 0x0000 - 0x01F0
+  * and a dummy address byte (0x00) will be added
+  */
+  void block4Erase(uint16_t block);
+
+  /** erase a block data(32Kbytes) on chip
+  * @param block number 0x00 - 0x03
+  * block number will be left-shifted by 3 bits: 0x00 - 0x18
+  * and two dummy address bytes (0x0000) will be added
+  */
+  void block32Erase(uint8_t block);
+  void chipErase();  //! erase all data on chip
+
+ private:
+  void writeEnable();   //! write enable
+  void writeDisable();  //! write disable
+  void chipEnable();    //! chip enable
+  void chipDisable();   //! chip disable
+
+  //! SPI _spi;
+  DigitalInOut _cs;
 };
 
-#endif
+#endif  // W25X40BV_H_
